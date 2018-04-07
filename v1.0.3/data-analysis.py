250352bdcbbd9e75eb1387e39385dde168eed8d3
2bPro <b.prodan95@gmail.com>
2018-04-07 10:38:24 +0100

    create tfidf matrix

--- v1.0.3/data-analysis.py
+++ v1.0.3/data-analysis.py
@@ -9,10 +9,12 @@ from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem import WordNetLemmatizer, PorterStemmer
 from nltk.probability import FreqDist
 
+from sklearn.feature_extraction.text import TfidfVectorizer
+
 from string import punctuation
 from collections import Counter
                 
-def contentProcessing(text):
+def tokenizer(text):
     # Drop symbols
     job_text = re.sub("[^a-zA-Z.+]", "  ", text)
 
@@ -78,16 +80,38 @@ if __name__ == '__main__':
     #data.category.value_counts().plot(kind='bar', grid=True, figsize=(3, 5))
     #pylab.show()
 
-    tokens = data['descr'].map(contentProcessing)
+    tokens = data['descr'].map(tokenizer)
 
     data['tokens'] = tokens
 
-    for descr, tokens in zip(data['descr'].head(5), data['tokens'].head(5)):
-        print('description:', descr)
-        print('tokens:', tokens)
-        print()
+    # Print job tokens list
+    #for descr, tokens in zip(data['descr'].head(5), data['tokens'].head(5)):
+    #    print('description:', descr)
+    #    print('tokens:', tokens)
+    #    print()
 
-    for category in set(data['category']):
-        print('category:', category)
-        print('top 10 keywords:', keywords(category))
-        print('---')
+    # Print top ten keywords for every job category    
+    #for category in set(data['category']):
+    #    print('category:', category)
+    #    print('top 10 keywords:', keywords(category))
+    #    print('---')
+
+    # Create tfidf matrix
+    vectorizer = TfidfVectorizer(min_df=10, max_features=10000, tokenizer=tokenizer, ngram_range=(1, 2))
+    vz = vectorizer.fit_transform(list(data['descr']))
+    vz.shape
+
+    tfidf = dict(zip(vectorizer.get_feature_names(), vectorizer.idf_))
+    tfidf = pd.DataFrame(columns=['tfidf']).from_dict(dict(tfidf), orient='index')
+    tfidf.columns = ['tfidf']
+
+    # Histogram showing distribution of tfidf scores
+    #tfidf.tfidf.hist(bins=50, figsize=(15,7))
+
+    # Top 30 tokens with the lowest tfidf scores
+    print tfidf.sort_values(by=['tfidf'], ascending=True).head(30)
+
+    # Top 30 tokens with the highest tfidf scores
+    print tfidf.sort_values(by=['tfidf'], ascending=False).head(30)
+
+    pylab.show()
